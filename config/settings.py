31e6b72d2af8360eb27fa419fad92ccd0f90cf8e
Riddlerman <me@somewhere>
2016-12-02 14:02:28 +1100

    Initial Commit

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'Itri3d2pickaB3tter$ecret!'
+
+APP_NAME = 'Moby Dock'
+
+DEBUG = True
+TESTING = False
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://mobydock:yourpassword@postgres:5432/mobydock'
+REDIS_URL = 'redis://redis:6379/0'
